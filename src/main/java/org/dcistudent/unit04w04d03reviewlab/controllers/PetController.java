574ac8d76abcd717833223d9e0bd907086440444
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-05-22 12:18:56 +0200

    Implement controller

--- src/main/java/org/dcistudent/unit04w04d03reviewlab/controllers/PetController.java
+++ src/main/java/org/dcistudent/unit04w04d03reviewlab/controllers/PetController.java
@@ -1,4 +1,53 @@
 package org.dcistudent.unit04w04d03reviewlab.controllers;
 
+import org.dcistudent.unit04w04d03reviewlab.entities.Pet;
+import org.dcistudent.unit04w04d03reviewlab.services.PetService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PetController {
+
+  PetService service;
+
+  public PetController(PetService service) {
+    this.service = service;
+  }
+
+  @GetMapping("/pets")
+  public List<Pet> getAll() {
+    return this.service.getAll();
+  }
+
+  @GetMapping("/pets/new")
+  public Pet create(
+      @PathVariable String name,
+      @PathVariable String breed,
+      @PathVariable int age
+  ) {
+    return this.service.create(name, breed, age);
+  }
+
+  @GetMapping("/pets/{id}")
+  public Pet getById(@PathVariable Long id) {
+    return this.service.getById(id);
+  }
+
+  @GetMapping("/pets/{id}/edit")
+  public Pet edit(
+      @PathVariable Long id,
+      @PathVariable String name,
+      @PathVariable String breed,
+      @PathVariable int age
+  ) {
+    return this.service.edit(id, name, breed, age);
+  }
+
+  @GetMapping("/pets/{id}/delete")
+  public Boolean delete(@PathVariable Long id) {
+    return this.service.delete(id);
+  }
 }
