2bceb3d816056cd1cb0df32f2ca5ff94080362b9
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-05-22 12:18:41 +0200

    Implement repository

--- src/main/java/org/dcistudent/unit04w04d03reviewlab/repositories/PetRepository.java
+++ src/main/java/org/dcistudent/unit04w04d03reviewlab/repositories/PetRepository.java
@@ -2,11 +2,6 @@ package org.dcistudent.unit04w04d03reviewlab.repositories;
 
 import org.dcistudent.unit04w04d03reviewlab.entities.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
-  @Query("SELECT p FROM Pet AS p")
-  public List<Pet> findAll();
 }
